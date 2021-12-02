0ee2c2ba3f5bf3ff52c9a137cd4334588e620751
Michael Fazio <michael@sandstone.io>
2021-12-03 00:58:33 +0800

    Fixed NavBar responsiveness

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,21 +1,21 @@
 <template>
-  <Disclosure as="nav" class="fixed w-screen z-50 pl-4 pr-4 bg-white shadow-xl" v-slot="{ open }">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
+  <Disclosure as="nav" class="fixed w-screen z-50 pl-4 pr-4 bg-white shadow-xl left-0 right-0" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
           <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="sr-only">Open main menu</span>
             <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
-        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+        <div class="flex-1 flex items-center justify-center md:items-stretch md:justify-between">
           <div class="flex-shrink-0 flex items-center">
             <a href="/#">
               <img class="h-6 w-auto sm:h-6" src="~/assets/images/sandstone-logo.svg" alt=""/>
             </a>
           </div>
-          <div class="hidden sm:block sm:ml-6">
+          <div class="hidden md:block sm:ml-6">
             <div class="flex space-x-4">
               <a v-for="item in navigation" :key="item.name" :href="item.anchor" class="text-blue-dark hover:text-blue-light hover:bg-blue-900 px-3 py-2 rounded-md text-sm font-medium">{{ item.name }}</a>
             </div>
@@ -23,7 +23,7 @@
         </div>
       </div>
     </div>
-    <DisclosurePanel class="sm:hidden">
+    <DisclosurePanel class="md:hidden">
       <div class="px-2 pt-2 pb-3 space-y-1">
         <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.anchor" class="text-blue-dark hover:text-blue-light hover:bg-blue-900 block px-3 py-2 rounded-md text-base cursor-pointer" >{{ item.name }}</DisclosureButton>
       </div>
