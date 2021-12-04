8f1369e76b80d9061a65a4ec2682c4db369f88c8
Michael Fazio <michael@sandstone.io>
2021-12-05 03:14:22 +0800

    Revert to default netlify configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-    target: "server",
-    nitro: {
-        preset: "lambda"
-    },
     build: {
         postcss: {
             postcssOptions: {
