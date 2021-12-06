4b9aabaecf195744cbc17fc8861f939099ac5aa9
Michael Fazio <michael@sandstone.io>
2021-12-06 17:05:13 +0800

    Removed overly specific configuration settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-    nitro: {
-        preset: "netlify"
-    },
-    ssr: true,
-    target: "static",
     components: true,
     build: {
         postcss: {
