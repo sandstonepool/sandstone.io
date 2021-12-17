0602f8d8562b8367d103cc40cb230a86b2d4fe81
Michael Fazio <michael@sandstone.io>
2021-12-17 19:59:21 +0800

    Added theme colour to meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,7 @@
       <Meta charset="UTF-8"></Meta>
       <Meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"></Meta>
       <Meta name="description" content="The most trusted and reliable stake pool on the Cardano blockchain"></Meta>
+      <Meta name="theme-color" content="#0437ffff"></Meta>
       <Link rel="shortcut icon" type="image/x-icon" href="/images/sandstone-64x64.png"/>
       <Link rel="apple-touch-icon" type="image/x-icon" href="/images/sandstone-64x64.png"/>
     </Head>
