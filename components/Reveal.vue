1f6ca97b4da2583c7d62e913c5009ac3585617fb
Michael Fazio <michael@sandstone.io>
2021-12-17 20:42:06 +0800

    Make disclosure panel client only in reveal component

--- components/Reveal.vue
+++ components/Reveal.vue
@@ -1,23 +1,25 @@
 <template>
   <Box>
-    <Disclosure as="div" class="p-4" v-slot="{ open }">
-      <DisclosureButton
-        class="inline-flex justify-between w-full px-4 py-2 text-md font-medium text-left rounded-lg focus:outline-none">
-        <span>{{ summary }}</span>
-        <ChevronUpIcon :class="open ? 'transform rotate-180' : ''" class="flex-shrink-0 h-8 w-8 text-blue-dark"/>
-      </DisclosureButton>
-      <transition
-        enter-active-class="transition duration-400 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-out"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0">
-        <DisclosurePanel class="px-4 pt-4 pb-2">
-          <slot></slot>
-        </DisclosurePanel>
-      </transition>
-    </Disclosure>
+    <ClientOnly>
+      <Disclosure as="div" class="p-4" v-slot="{ open }">
+        <DisclosureButton
+          class="inline-flex justify-between w-full px-4 py-2 text-md font-medium text-left rounded-lg focus:outline-none">
+          <span>{{ summary }}</span>
+          <ChevronUpIcon :class="open ? 'transform rotate-180' : ''" class="flex-shrink-0 h-8 w-8 text-blue-dark"/>
+        </DisclosureButton>
+        <transition
+          enter-active-class="transition duration-400 ease-out"
+          enter-from-class="transform scale-95 opacity-0"
+          enter-to-class="transform scale-100 opacity-100"
+          leave-active-class="transition duration-75 ease-out"
+          leave-from-class="transform scale-100 opacity-100"
+          leave-to-class="transform scale-95 opacity-0">
+          <DisclosurePanel class="px-4 pt-4 pb-2">
+            <slot></slot>
+          </DisclosurePanel>
+        </transition>
+      </Disclosure>
+    </ClientOnly>
   </Box>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import {defineNuxtConfig} from 'nuxt3'
 export default defineNuxtConfig({
   components: true,
   build: {
+    aggressiveCodeRemoval: true,
+    optimizeCSS: true,
+    transpile: [
+      "@heroicons/vue"
+    ],
     postcss: {
       postcssOptions: {
         plugins: {
