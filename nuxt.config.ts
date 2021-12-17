d301d7bc4573bae5667a0b99be187f952df334b3
Michael Fazio <michael@sandstone.io>
2021-12-17 20:47:39 +0800

    Added build time compression

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@ import {defineNuxtConfig} from 'nuxt3'
 
 export default defineNuxtConfig({
   components: true,
+  buildModules: [
+    ['nuxt-compress', {
+      gzip: {
+        threshold: 8192,
+      },
+      brotli: {
+        threshold: 8192,
+      },
+    }]
+  ],
   build: {
     aggressiveCodeRemoval: true,
     optimizeCSS: true,
